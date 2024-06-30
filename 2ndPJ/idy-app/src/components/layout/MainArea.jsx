8801a0a56494ca980b93c5ca48ec05f3ff9128ed
doyeun93 <doyeun93@naver.com>
2024-06-30 20:52:28 +0900

    s

--- 2ndPJ/idy-app/src/components/layout/MainArea.jsx
+++ 2ndPJ/idy-app/src/components/layout/MainArea.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom';
 
+
 function MainArea(props) {
     return (
         <main id="main-area"> 
-            <Outlet />
+            <Outlet /> 
         </main>
     );
 }
