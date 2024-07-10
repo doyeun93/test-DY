39dca9efa8bdc9383b016eadf4c2dc3e676fea70
doyeun93 <doyeun93@naver.com>
2024-07-10 18:06:01 +0900

    s

--- 2ndPJ/idy-app/src/components/modules/Showmenu.jsx
+++ 2ndPJ/idy-app/src/components/modules/Showmenu.jsx
@@ -19,14 +19,18 @@ export function ShowMenu() {
           <ul className="htit">
             <li onClick={goSub}>새미네소개</li>
             <li onClick={goSub}>요리초보가이드</li>
-            <ul className="htext">
-              <li onClick={goSub}>레시피</li>
-              <li onClick={goSub}>솔루션</li>
-            </ul>
-            <ul className="htext">
-              <li onClick={goSub}>요리해요</li>
-              <li onClick={goSub}>질문있어요</li>
-            </ul>
+            <li onClick={goSub}>요리연구소
+              <ul className="htext">
+                <li onClick={goSub}>레시피</li>
+              < li onClick={goSub}>솔루션</li>
+              </ul>
+            </li>
+            <li onClick={goSub}>요리해요
+              <ul className="htext">
+                <li onClick={goSub}>요리해요</li>
+              < li onClick={goSub}>질문있어요</li>
+              </ul>
+            </li>
             <li onClick={goSub}>WOW이벤트</li>
           </ul>
         </nav>
