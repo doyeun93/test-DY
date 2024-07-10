40c2431a7f13f90025e351d5f1a43f0356e87bd2
doyeun93 <doyeun93@naver.com>
2024-07-10 12:32:34 +0900

    s

--- 2ndPJ/idy-app/src/components/layout/Layout.jsx
+++ 2ndPJ/idy-app/src/components/layout/Layout.jsx
@@ -1,21 +1,71 @@
 // 전체 레이아웃 컴포넌트 ///
-import React from "react";
-import TopArea from "./TopArea";
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {TopArea} from "./TopArea";
 import MainArea from "./MainArea";
 import FooterArea from "./FooterArea";
 
+import { dCon } from "../modules/dCon";
 
 function Layout() {
+  
+    // 상태관리 변수 ///
+    // 1. 로그인 상태관리 변수 -> 초기값으로 세션스토리지 "minfo"를 할당함
+    const [loginSts, setLoginSts] = 
+    useState(sessionStorage.getItem("minfo"));
+
+
+    // 2. 로그인 환영 메시지 상태변수
+    const [loginMsg, setLoginMsg] = useState(null);
+
+    // [ 공통 함수]
+    // 1. 라우팅 이동 함수 : 라우팅 이동후크인 useNavigate는 
+    // 다른 useCallback() 후크로 처리할 수 없다. 따라서 별도의 함수를 만들고 콜백처리해준다
+    // -> const goPage = useCallback
+    const goNav = useNavigate();
+
+    // 함수메모처리위해 useCallback()에 넣어준다 -> 기억해놨다가 나중에 불러와서 쓰려고
+    const goPage = useCallback((pm1, pm2) => {
+        goNav(pm1,pm2);
+    }, []);
+
+    // 2. 로그인 환영메시지 생성함수
+    const makeMsg = useCallback((name) => {
+        let usrIcon = ["🙍‍♂","🧏‍♀","🦸‍♂","👨‍🎤","🦸‍♀"];
+        let rdm = Math.floor(Math.random() *5);
+        setLoginMsg(`Welcome ${name} ${usrIcon[rdm]}`);
+    },[]); ////// makeMsg 함수 ///////////
+
+    // 3. 로그아웃 함수
+    const logoutFn = useCallback(() => {
+        setLoginSts(null);
+        sessionStorage.removeItem("minfo");
+        setLoginMsg(null);
+        goPage("/");
+
+    },[]) /////// logoutFn 함수 /////////
+
+     // 4. 로그인 상태 체크함수 -> 화면 랜더링 상태 체크 
+     useEffect(()=>{
+        if(sessionStorage.getItem("minfo")){
+            let ss = sessionStorage.getItem("minfo");
+            setLoginSts(ss);
+            makeMsg(JSON.parse(ss).unm);
+            
+        } ////// if ///////
+
+     },[]);
+
+
   return (
-    <>
-      
+    <dCon.Provider value={{loginSts, setLoginSts,loginMsg,setLoginMsg,goPage,makeMsg,logoutFn}}>
       {/* 1.상단영역 */}
-      <TopArea />
+      <TopArea loginMsg={loginMsg} loginSts={loginSts} logoutFn={logoutFn} goPage={goPage}/>
       {/* 2.메인영역 */}
       <MainArea />
       {/* 3.하단영역 */}
       <FooterArea />
-    </>
+    </dCon.Provider>
   );
 }
 
--- 2ndPJ/idy-app/src/components/layout/TopArea.jsx
+++ 2ndPJ/idy-app/src/components/layout/TopArea.jsx
@@ -1,120 +1,156 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { memo } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { gnbData } from '../data/gnb';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { gnbData } from "../data/gnb";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "../../css/top_area.scss";
 
-import $ from 'jquery';
+import $ from "jquery";
 
 
-
-function TopArea(props) {
-
+export const TopArea = memo(({loginMsg,loginSts,logoutFn,goPage}) =>{
   const goNav = useNavigate();
 
   // 햄버거버튼
   const ShowMenu = () => {
     $(".ham").toggleClass("on");
-      $(".hbox").show();
-  }
+    $(".hbox").show();
+  };
 
-   // 1. 검색창 보이기 함수
-   const showSearch = (e) => {
+  // 1. 검색창 보이기 함수
+  const showSearch = (e) => {
     e.preventDefault();
     $(".searchingGnb").show();
     $("#schinGnb").focus();
-  
   }; /////// showSearch /////////////////////////////////
- 
+
   const enterKey = (e) => {
-    console.log(e.key,e.keyCode);
-    if(e.key == "Enter"){
+    console.log(e.key, e.keyCode);
+    if (e.key == "Enter") {
       let txt = $(e.target).val().trim();
       console.log(txt);
-      
-      if(txt!=''){
-       $(e.target).val("").parent().hide();
+
+      if (txt != "") {
+        $(e.target).val("").parent().hide();
         goSearch(txt);
       }
     } // if
-
   }; /////// enterKey ///////////////////
 
-  const goSearch = txt => {
-    goNav("search",{state:{keyword:txt}})
+  const goSearch = (txt) => {
+    goNav("search", { state: { keyword: txt } });
   }; /////////////// goSearch //////////
 
-    return (
-        <section id="top-area">
-          <header className="topbox">
-              <a className="logo" href="#" >
-              <img src={process.env.PUBLIC_URL+"/image/logo.jpg"} alt="로고이미지" 
-                onClick={(e) => {e.preventDefault(); 
-                goNav("/")}}/>
-              </a>
-            <nav id="gnb">
-              <ul className="top-gnb">
-                {gnbData.map((v,i)=>
-                <li key={i}>
-                  {
-                    v.sub ? <a href="#">{v.txt}</a> : <Link to={v.link}>{v.txt}</Link>
-                  }
-                  {
-                    v.sub && <ul className="bt-gnb">
-                      {
-                        v.sub.map((v,i)=>
-                        <li key={i}>
-                          <Link to={v.link}>{v.txt}</Link>
-                        </li>)
-                      }
-                    </ul>
-                  }
-                </li>)}
-              </ul>
-              <button className="ham" type="button" 
-              onClick={(e)=> {
-                e.preventDefault();
-                ShowMenu();
-                }}></button>
-            
-            </nav>
-            <div className="log">
-              <ul>
+  return (
+    <section id="top-area">
+      <header className="topbox">
+        <a className="logo" href="#">
+          <img
+            src={process.env.PUBLIC_URL + "/image/logo.jpg"}
+            alt="로고이미지"
+            onClick={(e) => {
+              e.preventDefault();
+              goNav("/");
+            }}
+          />
+        </a>
+        <nav id="gnb">
+          <ul className="top-gnb">
+            {gnbData.map((v, i) => (
+              <li key={i}>
+                {v.sub ? <a href="#">{v.txt}</a> : <Link to={v.link}>{v.txt}</Link>}
+                {v.sub && (
+                  <ul className="bt-gnb">
+                    {v.sub.map((v, i) => (
+                      <li key={i}>
+                        <Link to={v.link}>{v.txt}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+          <button
+            className="ham"
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              ShowMenu();
+            }}
+          ></button>
+        </nav>
+        <div className="log">
+          <ul>
+            {loginSts === null && (
+              <>
+                <li>
+                  <Link to="/member">회원가입</Link>
+                </li>
                 <li>
                   <Link to="/login">로그인</Link>
                 </li>
-                <li style={{color:"#15a775"}}>
-                {/* 검색입력박스 */}
-                <div className="searchingGnb" >
-                  {/* 검색버튼 돋보기 아이콘 */}
-                  <FontAwesomeIcon icon={faSearch} className="schbtnGnb" title="Open search"
-                   onClick={(e)=>{ 
+              </>
+            )}
+            {
+              /* 로그인 상태면 로그아웃버튼 보임 */
+              loginSts !== null && (
+                <>
+                  <li>
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        // 로그아웃처리함수 호출
+                        logoutFn();
+                      }}
+                    >
+                      LOGOUT
+                    </a>
+                  </li>
+                </>
+              )
+            }
+            <li style={{ color: "#15a775" }}>
+              {/* 검색입력박스 */}
+              <div className="searchingGnb">
+                {/* 검색버튼 돋보기 아이콘 */}
+                <FontAwesomeIcon
+                  icon={faSearch}
+                  className="schbtnGnb"
+                  title="Open search"
+                  onClick={(e) => {
                     // 검색어 읽기
                     let stxt = e.currentTarget.nextElementSibling.value;
-                    if(stxt.trim()!=""){
+                    if (stxt.trim() != "") {
                       // 검색하기
                       goSearch(stxt);
-                    }
-                    else{
+                    } else {
                       // 검색어 비었을 때 메시지
-                       alert("검색어를 입력하세요.");
+                      alert("검색어를 입력하세요.");
                     }
-                  }}/>
-                  {/* 입력창 */}
-                  <input type="text" name="schinGnb" id="schinGnb" placeholder="어떤 요리가 궁금하신가요?" onKeyUp={enterKey}/>
-                </div>
-                {/* 검색기능링크 - 클릭시 검색창 보이기 */}
-                <a href="#" onClick={showSearch}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </a>
+                  }}
+                />
+                {/* 입력창 */}
+                <input
+                  type="text"
+                  name="schinGnb"
+                  id="schinGnb"
+                  placeholder="어떤 요리가 궁금하신가요?"
+                  onKeyUp={enterKey}
+                />
+              </div>
+              {/* 검색기능링크 - 클릭시 검색창 보이기 */}
+              <a href="#" onClick={showSearch}>
+                <FontAwesomeIcon icon={faSearch} />
+              </a>
             </li>
-              </ul>
-            </div>
-          </header>
-        </section>
-    );
+          </ul>
+        </div>
+      </header>
+    </section>
+  );
 }
+)
 
-export default TopArea;
