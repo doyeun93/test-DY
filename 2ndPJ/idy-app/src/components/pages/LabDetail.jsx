83576c8645fec19c4c78c56c274e53c5cc84a603
doyeun93 <doyeun93@naver.com>
2024-07-17 17:43:48 +0900

    s

--- 2ndPJ/idy-app/src/components/pages/LabDetail.jsx
+++ 2ndPJ/idy-app/src/components/pages/LabDetail.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
+import "../../css/cooklab_detail.scss";
+import $ from "jquery";
+import {dCon} from "../modules/dCon";
 
 import { useLocation } from "react-router-dom";
 
-function LabDetail(props) {
-    const loc = useLocation();
-    const title = loc.state.title;
-    const text = loc.state.text;
-    const type = loc.state.type;
+function LabDetail({tot, dt, setTot}) {
+    // tot - 레시피 토탈 정보
+    // dt - 레시피데이터
+    // setTot - 레시피 토탈정보 업데이트 함수
+    let imgName = tot.imgName;
+    
 
     return (
-        <div>
-            
-        </div>
+        <>
+            {/* 레시피 상세정보 박스 */}
+            <div className="lab-detail">
+                {/* 레시피 상세정보 제목 */}
+                <div className="ldetail-top">
+
+                </div>
+                {/* 레시피 상세정보 소제목 */}
+                <div className="ldetail-tit">
+                    <h2></h2>
+                </div>
+                {/* 레시피 상세정보 이미지 */}
+                <div className="ldetail-pic">
+
+                </div>
+                {/* 레시피 상세정보 재료 */}
+                <div className="ldetail-txt">
+                    <div className="ldetail-txt1">
+                        {/* type1,2,3 들어감 */}
+                    </div>
+                    <div className="ldetail-txt2">
+                        {/* li에 type4,5,6 들어감 */}
+                        <ul> 
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+                {/* 레시피 상세정보 레시피 이미지 3개*/}
+                <div className="ldetail-img">
+
+                </div>
+                {/* 레시피 상세정보 레시피 순서 */}
+                <div className="ldetail-seq">
+                    {/* type7,8,9 들어감 */}
+                </div>
+            </div>
+        </>
     );
 }
 
