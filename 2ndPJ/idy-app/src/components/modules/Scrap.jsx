1beb9ed40edead29e83a1b38fd52ef334a2985b3
doyeun93 <doyeun93@naver.com>
2024-08-16 23:40:00 +0900

    Update Scrap.jsx

--- 2ndPJ/idy-app/src/components/modules/Scrap.jsx
+++ 2ndPJ/idy-app/src/components/modules/Scrap.jsx
@@ -14,10 +14,9 @@ function Scrap(props) {
   const dataCnt = selData.length;
 
   useEffect(() => {
-    // 카트 버튼 나타나기
     $("#myScrap")
       .removeClass("on")
-      .delay(300) // 애니메이션 지연시간
+      .delay(300) 
       .fadeIn(300, function () {
         $(this).addClass("on");
       }); //// fadeIn ///////
@@ -32,7 +31,6 @@ function Scrap(props) {
             className="cbtn cbtn2"
             onClick={(e) => {
               e.preventDefault();
-              // 오른쪽으로 이동하여 사라짐
               $("#scraplist").animate({ right: "-60vw" }, 400);
             }}
           >
@@ -111,23 +109,18 @@ function Scrap(props) {
                                   <button
                                     className="btn-insert"
                                     onClick={(e) => {
-                                      // 클릭시 실제 데이터 수량변경 반영하기
-                                      // 대상 : selData -> 배열 변환 데이터
-                                      // i는 배열 순번(map돌 때 i가 들어옴)
                                       selData[i].cnt = $(e.currentTarget).siblings(".item-cnt").val();
         
-                                      // 2. 데이터 문자화하기 : 변경된 원본을 문자화
                                       let res = JSON.stringify(selData);
-                                      // 3.로컬스 "scrap-data"반영하기
+                                      
                                       localStorage.setItem("scrap-data", res);
-                                      // 4. 카트리스트 전역상태변수 변경
+                                      
                                       myCon.setLocalsScrap(res);
-                                      // 5. 반영 버튼 숨기기
+                                   
                                       $(e.currentTarget)
                                         .hide()
-                                        .next() // "취소" 버튼
+                                        .next() 
                                         .hide();
-                                      // -> 따라서 이때 강제 리랜더링이 필요하다
                                       setForce(!force);
                                     }}
                                   >
@@ -139,11 +132,10 @@ function Scrap(props) {
                                     onClick={(e) => {
                                       $(e.currentTarget)
                                         .hide()
-                                        .prev() // "반영" 버튼
+                                        .prev() 
                                         .hide()
                                         .siblings("input")
                                         .val(v.cnt);
-                                      // 취소 버튼 자신의 css를 변경하고(안보이게) 형제요소 중 input을 찾아 값으로 기존값인 v.cnt를 넣는다
                                     }}
                                   >
                                     취소
@@ -151,29 +143,18 @@ function Scrap(props) {
                                   <b
                                     className="btn-cnt"
                                     onClick={(e) => {
-                                      // 업데이트 대상(input박스)
                                       let tg = $(e.currentTarget).siblings("input");
-                                      // 입력창의 blur 이벤트 발생을 위해 강제로 포커스를 준다
-                                      // tg.focus();
-                                      // 하위 클릭된 이미지 종류 파악하기 (currentTarget :이벤트가 걸린 요소 자신 ) / target : 하위요소
-                                      // e.target으로 설정하여 하위요소인 이미지가 선택되게 해준다
                                       let btnAlt = $(e.target).attr("alt");
-                                      console.log(btnAlt);
-                                      // 증가 감소 분기하여 숫자 변경반영하기
                                       if (btnAlt == "증가") {
-                                        // tg 값을 읽어와서 1을 더한다
                                         tg.val(Number(tg.val()) + 1);
-                                      } ////// if 문 ///////////
+                                      } 
                                       else if (btnAlt == "감소") {
-                                        // tg 값을 읽어와서 1을 뺀다
-                                        // 단, 1보다 작아지지 않게 한다
                                         tg.val(Number(tg.val()) == 1 ? 1 : Number(tg.val() - 1));
-                                      } ///// else if //////////
-                                      // 클릭시 반영, 취소 버튼 나타나기
+                                      }
                                       $(e.currentTarget)
                                         .siblings(".btn-insert")
                                         .show()
-                                        .next() // 취소 버튼
+                                        .next() 
                                         .show();
                                     }}
                                   >
@@ -190,34 +171,21 @@ function Scrap(props) {
                               </div>
                             </td>
                             <td>
-                              {/* <span className="sum-num1">{addComma(v.ginfo[3] * v.cnt)}</span>원 */}
-                              {/* 계산된 합계금액 숫자만 히든필드에 넣어놓고
-                                  총합계 계산에 사용함 */}
-                              {/* <input
-                                className="sum-num2"
-                                type="hidden"
-                                defaultValue={v.ginfo[3] * v.cnt}
-                              /> */}
                             </td>
                             <td>
                               {/* 데이터 삭제 기능 버튼 */}
                               <button
                                 className="cfn"
                                 onClick={() => {
-                                  // confirm()의 "확인" 클릭시 true -> console이 찍힘
                                   if (window.confirm("해당 상품을 삭제하시겠습니까?")) {
-                                    // 지울 배열 순번은 map()에서 i로 들어옴
-                                    // 지울 배열은 selData임
-                                    // 1. 데이터 지우기
+                                    
                                     selData.splice(i, 1);
-                                    // 2. 데이터 문자화하기 : 변경된 원본을 문자화함
                                     let res = JSON.stringify(selData);
-                                    // 3. 로컬스 "scrap-data"반영하기
+                                    
                                     localStorage.setItem("scrap-data", res);
-                                    // 4. 카트리스트 전역상태변수 변경 -> 리랜더링 하기위함
+                                    
                                     myCon.setLocalsScrap(res);
-                                    // 5. 데이터개수가 0이면 카트 리스트
-                                    // 상태변수를 false로 변경하여 카트 리스트 출력을 없앤다
+                              
                                     if (selData.length == 0) myCon.setScrapSts(false);
                                   } ///// if //////
                                 }}
